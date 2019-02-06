90da93f238fa76e57c66f10843da51d8b6510606
Vasilliy Dio <xxxthiafxxx@gmail.com>
2019-02-06 11:50:58 +0300

    06.02

--- app/libs/Табы/Tabs.js
+++
@@ -1,5 +0,0 @@
-$(".tab_block").not(":first").hide(); // спрятать все tab_item кроме первого
-$(".tab_wrapper .menu_item").click(function(){
-    $(".tab_wrapper .menu_item").removeClass("active").eq($(this).index()).addClass("active");
-    $(".tab_block").hide().eq($(this).index()).fadeIn(800)
-}).eq(0).addClass("active");
